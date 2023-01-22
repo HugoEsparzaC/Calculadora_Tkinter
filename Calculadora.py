60d0d46c6bced155750477389a2b619f84543c15
HugoEsparzaC <hugo.esparza3@gmail.com>
2023-01-21 19:57:27 -0600

    Todos los botones

--- Calculadora.py
+++ Calculadora.py
@@ -5,7 +5,7 @@ class Calculadora(tk.Tk):
     def __init__(self):
         super().__init__()
         self.geometry('400x450')
-        # self.resizable(0,0)
+        self.resizable(0,0)
         self.title('Calculadora')
         self.iconbitmap('cal.ico')
         self.expresion = ''
@@ -30,8 +30,33 @@ class Calculadora(tk.Tk):
         boton_ocho.grid(row=1, column=1, padx=1, pady=1)
         boton_nueve = tk.Button(botones_frame, text='9', width=10, height=3, bd=0, bg='#fff', cursor='hand2', command=lambda: self._evento_click(9))
         boton_nueve.grid(row=1, column=2, padx=1, pady=1)
-        boton_multiplicar = tk.Button(botones_frame, text='X', width=10, height=3, bd=0, bg='#fff', cursor='hand2', command=lambda: self._evento_click('X'))
+        boton_multiplicar = tk.Button(botones_frame, text='X', width=10, height=3, bd=0, bg='#eee', cursor='hand2', command=lambda: self._evento_click('X'))
         boton_multiplicar.grid(row=1, column=3, padx=1, pady=1)
+
+        boton_cuatro = tk.Button(botones_frame, text='4', width=10, height=3, bd=0, bg='#fff', cursor='hand2', command=lambda: self._evento_click(4))
+        boton_cuatro.grid(row=2, column=0, padx=1, pady=1)
+        boton_cinco = tk.Button(botones_frame, text='5', width=10, height=3, bd=0, bg='#fff', cursor='hand2', command=lambda: self._evento_click(5))
+        boton_cinco.grid(row=2, column=1, padx=1, pady=1)
+        boton_seis = tk.Button(botones_frame, text='6', width=10, height=3, bd=0, bg='#fff', cursor='hand2', command=lambda: self._evento_click(6))
+        boton_seis.grid(row=2, column=2, padx=1, pady=1)
+        boton_restar = tk.Button(botones_frame, text='-', width=10, height=3, bd=0, bg='#eee', cursor='hand2', command=lambda: self._evento_click('-'))
+        boton_restar.grid(row=2, column=3, padx=1, pady=1)
+
+        boton_uno = tk.Button(botones_frame, text='1', width=10, height=3, bd=0, bg='#fff', cursor='hand2', command=lambda: self._evento_click(1))
+        boton_uno.grid(row=3, column=0, padx=1, pady=1)
+        boton_dos = tk.Button(botones_frame, text='2', width=10, height=3, bd=0, bg='#fff', cursor='hand2', command=lambda: self._evento_click(2))
+        boton_dos.grid(row=3, column=1, padx=1, pady=1)
+        boton_tres = tk.Button(botones_frame, text='3', width=10, height=3, bd=0, bg='#fff', cursor='hand2', command=lambda: self._evento_click(3))
+        boton_tres.grid(row=3, column=2, padx=1, pady=1)
+        boton_sumar = tk.Button(botones_frame, text='+', width=10, height=3, bd=0, bg='#eee', cursor='hand2', command=lambda: self._evento_click('+'))
+        boton_sumar.grid(row=3, column=3, padx=1, pady=1)
+
+        boton_cero = tk.Button(botones_frame, text='0', width=21, height=3, bd=0, bg='#fff', cursor='hand2', command=lambda: self._evento_click(0))
+        boton_cero.grid(row=4, column=0, columnspan=2, padx=1, pady=1)
+        boton_punto = tk.Button(botones_frame, text='.', width=10, height=3, bd=0, bg='#eee', cursor='hand2', command=lambda: self._evento_click('.'))
+        boton_punto.grid(row=4, column=2, padx=1, pady=1)
+        boton_igual = tk.Button(botones_frame, text='=', width=10, height=3, bd=0, bg='#eee', cursor='hand2', command=lambda: self._evento_click('='))
+        boton_igual.grid(row=4, column=3, padx=1, pady=1)
 
     def _evento_limpiar(self):
         self.expresion = ''
