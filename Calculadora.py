8b877f890cb4afc90902967e90af0b1e124914a8
HugoEsparzaC <hugo.esparza3@gmail.com>
2023-01-21 19:42:57 -0600

    botones 7, 8, 9, X

--- Calculadora.py
+++ Calculadora.py
@@ -5,7 +5,7 @@ class Calculadora(tk.Tk):
     def __init__(self):
         super().__init__()
         self.geometry('400x450')
-        self.resizable(0,0)
+        # self.resizable(0,0)
         self.title('Calculadora')
         self.iconbitmap('cal.ico')
         self.expresion = ''
@@ -22,9 +22,24 @@ class Calculadora(tk.Tk):
         botones_frame.pack()
         boton_limpiar = tk.Button(botones_frame, text='C', width=32, height=3, bd=0, bg='#eee', cursor='hand2', command=self._evento_limpiar)
         boton_limpiar.grid(row=0, column=0, columnspan=3, padx=1, pady=1)
+        boton_dividir = tk.Button(botones_frame, text='/', width=10, height=3, bd=0, bg='#eee', cursor='hand2', command=lambda: self._evento_click('/'))
+        boton_dividir.grid(row=0, column=3, padx=1, pady=1)
+        boton_siete = tk.Button(botones_frame, text='7', width=10, height=3, bd=0, bg='#fff', cursor='hand2', command=lambda: self._evento_click(7))
+        boton_siete.grid(row=1, column=0, padx=1, pady=1)
+        boton_ocho = tk.Button(botones_frame, text='8', width=10, height=3, bd=0, bg='#fff', cursor='hand2', command=lambda: self._evento_click(8))
+        boton_ocho.grid(row=1, column=1, padx=1, pady=1)
+        boton_nueve = tk.Button(botones_frame, text='9', width=10, height=3, bd=0, bg='#fff', cursor='hand2', command=lambda: self._evento_click(9))
+        boton_nueve.grid(row=1, column=2, padx=1, pady=1)
+        boton_multiplicar = tk.Button(botones_frame, text='X', width=10, height=3, bd=0, bg='#fff', cursor='hand2', command=lambda: self._evento_click('X'))
+        boton_multiplicar.grid(row=1, column=3, padx=1, pady=1)
 
     def _evento_limpiar(self):
-        pass
+        self.expresion = ''
+        self.entrada_texto.set(self.expresion)
+
+    def _evento_click(self, elemento):
+        self.expresion = f'{self.expresion}{elemento}'
+        self.entrada_texto.set(self.expresion)
 
 if __name__ == '__main__':
     calculadora = Calculadora()
