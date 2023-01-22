db7618baf968a79e686fbaa2e0b42a18320c34fc
HugoEsparzaC <hugo.esparza3@gmail.com>
2023-01-21 18:57:57 -0600

    Boton C

--- Calculadora.py
+++ Calculadora.py
@@ -18,6 +18,13 @@ class Calculadora(tk.Tk):
         entrada_frame.pack(side = tk.TOP)
         entrada = tk.Entry(entrada_frame, font=('arial', 18, 'bold'), textvariable = self.entrada_texto, width = 30, justify = tk.RIGHT)
         entrada.grid(row = 0, column = 0, ipady = 10)
+        botones_frame = tk.Frame(self, width=400, height=450, bg='grey')
+        botones_frame.pack()
+        boton_limpiar = tk.Button(botones_frame, text='C', width=32, height=3, bd=0, bg='#eee', cursor='hand2', command=self._evento_limpiar)
+        boton_limpiar.grid(row=0, column=0, columnspan=3, padx=1, pady=1)
+
+    def _evento_limpiar(self):
+        pass
 
 if __name__ == '__main__':
     calculadora = Calculadora()
