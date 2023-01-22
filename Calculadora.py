e8a1d9dc8477352f8b21ce6bc842111c27305b26
HugoEsparzaC <hugo.esparza3@gmail.com>
2023-01-21 18:42:58 -0600

    Cuadro de texto

--- Calculadora.py
+++ Calculadora.py
@@ -8,6 +8,16 @@ class Calculadora(tk.Tk):
         self.resizable(0,0)
         self.title('Calculadora')
         self.iconbitmap('cal.ico')
+        self.expresion = ''
+        self.entrada = None
+        self.entrada_texto = tk.StringVar()
+        self._creacion_componentes()
+
+    def _creacion_componentes(self):
+        entrada_frame = tk.Frame(self, width = 400, height = 50, bg = 'grey')
+        entrada_frame.pack(side = tk.TOP)
+        entrada = tk.Entry(entrada_frame, font=('arial', 18, 'bold'), textvariable = self.entrada_texto, width = 30, justify = tk.RIGHT)
+        entrada.grid(row = 0, column = 0, ipady = 10)
 
 if __name__ == '__main__':
     calculadora = Calculadora()
